37471e647be1657b6bf69a96da95426cabb0ec56
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2023-11-18 11:10:43 +0100

    taylor subido

--- taylor.py
+++ taylor.py
@@ -10,46 +10,63 @@ def euler(f, x0, y0, h, num_steps):
         v.append(y0)
     return u, v
 
-def taylor_second_order(f, df_dy, x0, y0, h, num_steps):
+def taylor(f, df_dx, df_dy, x, y, h, num_steps):
+    
     u = []
     v = []
     for i in range(num_steps):
-        # Método de Euler para obtener y_{i+1}
-        y0 = y0 + h * f(x0, y0)
-
-        # Método de Taylor de segundo orden para obtener y_{i+1}
-        y0 = y0 + h * (f(x0, y0) + 0.5 * h * df_dy(x0, y0))
-
-        x0 = x0 + h
-        u.append(x0)
-        v.append(y0)
+        y = y + h * f(x, y) + (h**2/2) * (df_dx(x, y) + df_dy(x, y) * f(x, y))
+        x = x + h
+        u.append(x)
+        v.append(y)
     return u, v
 
 def f(x, y):
-    return (1 + 4*x*y) / (3*x*x)
+    return (1 + 4*x*y)/(3*x**2)
+
+def df_dx(x, y):
+    return -(4*x*y + 2)/(3*x**3)
 
 def df_dy(x, y):
-    return 4 / (3 * x)
+    return (4)/(3*x)
+
+def error(v, v_aprox):
+    return abs(v - v_aprox)
+
+# datos
+x = 0.5
+y = -1
+h = 0.035
+num_steps = 100
 
 # Definir la región R
 x_min, x_max = 0.5, 4.0
 y_min, y_max = -3.0, 3.0
 
-# Condiciones iniciales
-x0 = 0.5
-y0 = -1
-h = 0.035
-num_steps = 100
+# Aplicamos el método de Taylor
+u_taylor, v_taylor = taylor(f, df_dx, df_dy, x, y, h, num_steps)
 
-# Solución utilizando el método de Euler
-u_euler, v_euler = euler(f, x0, y0, h, num_steps)
+# Imprimimos la última y del bucle
+print('w_100 (Taylor): ', v_taylor[-1])
 
-# Solución utilizando el método de Taylor de segundo orden
-u_taylor, v_taylor = taylor_second_order(f, df_dy, x0, y0, h, num_steps)
+# Error
+v_e = -11.46
+error_taylor = error(v_e, v_taylor[-1])
+print('Error (Taylor): ', error_taylor)
+
+# Aplicamos el método de Euler para comparar
+u_euler, v_euler = euler(f, x, y, h, num_steps)
+
+# Imprimimos la última y del bucle
+print('w_100 (Euler): ', v_euler[-1])
+
+# Error
+error_euler = error(v_e, v_euler[-1])
+print('Error (Euler): ', error_euler)
 
 # Graficar ambas soluciones
-plt.plot(u_euler, v_euler, label='Solución Real (Euler)')
-plt.plot(u_taylor, v_taylor, label='Solución Numérica (Taylor 2º orden)')
+plt.plot(u_taylor, v_taylor, label='Solución Numérica (Taylor)')
+plt.plot(u_euler, v_euler, label='Solución Numérica (Euler)')
 
 # Líneas verticales que representan los límites de la región R
 plt.axvline(x=x_min, color='r', linestyle='--', label='Límite inferior de x')
@@ -62,10 +79,6 @@ plt.axhline(y=y_max, color='g', linestyle='--', label='Límite superior de y')
 plt.legend()
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title('Comparación entre Solución Real y Solución Numérica')
+plt.title('Comparación entre Solución Numérica (Euler y Taylor)')
 plt.grid(True)
 plt.show()
-
-# Imprimir el valor de w_100 para ambas soluciones
-print(f"El valor aproximado de w_100 (Euler) es {v_euler[-1]}")
-print(f"El valor aproximado de w_100 (Taylor 2º orden) es {v_taylor[-1]}")
